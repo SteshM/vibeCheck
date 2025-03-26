31bba96e7496d3ff5307f959e6715ac7eba6fab5
SteshM <stella.mutua@imali.click>
2025-03-26 14:25:08 +0300

    Initial commit

---
+++ src/main/java/com/stesh/vibeCheck/VibeCheckApplication.java
@@ -0,0 +1,13 @@
+package com.stesh.vibeCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stesh/vibeCheck/VibeCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stesh.vibeCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
